8197a49773024244b0490388131fc576d27b7188
ifubaraboye <ifubaraboyeoribime@outlook.com>
2025-10-20 22:35:01 +0100

    added sidebar

--- src/app/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return(
-    <div className="sticky top-0 z-10">
-        <div className="max-w-3xl mx-auto px-6 py-5">
-            <h1 className="text-lg font-medium text-white">Gemma</h1>
-        </div>
-    </div>
-    )
-}
