d63f553080a8034945405810baf7301c845706de
ifubaraboye <ifubaraboyeoribime@outlook.com>
2025-10-21 21:16:02 +0100

    modified file

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -266,7 +266,7 @@ function SidebarTrigger({
       data-slot="sidebar-trigger"
       variant="ghost"
       size="icon"
-      className={cn("size-7 text-white", className)}
+      className={cn("size-7 cursor-pointer text-white", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
